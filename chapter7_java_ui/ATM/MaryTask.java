9a06489c682a7a16fb832c6036f89b33bb1e2bbb
ACupofAir <timethoughts@outlook.com>
2020-12-14 21:12:26 +0800

    Finish JavaL07

--- chapter7_java_ui/ATM/MaryTask.java
+++ chapter7_java_ui/ATM/MaryTask.java
@@ -1,12 +1,12 @@
 package chapter7_java_ui.ATM;
                                                                  
 public class MaryTask implements Runnable {
-  ATM o;         
-                 
+  ATM o;           
+                   
   public MaryTask(ATM account) {                                  
     o = account;                                                 
-  }              
-                 
+  }                
+                   
   public void run() {
     o.deposit(1500);
     o.withdraw(100);
--- chapter8_java_muti_thread/MyTask.java
+++ chapter8_java_muti_thread/MyTask.java
@@ -2,36 +2,36 @@ package chapter8_java_muti_thread;
 
 import java.util.ArrayList;
 
-public class MyTask implements Runnable {         
-  public synchronized void run() {                
+public class MyTask implements Runnable {
+  public synchronized void run() {
     String name = Thread.currentThread().getName();
-    System.out.println(name + "started.");         
-    try{                                           
-      wait(1000);                                           
-    }catch(Exception e) {                                   
-      e.printStackTrace();                                  
-    }                                                       
-    System.out.println(name + "ended.");                    
-  }                                                         
-                                                            
-  public static void main(String[] args) {                  
-    long begin = System.currentTimeMillis();                
-    MyTask task = new MyTask();                             
+    System.out.println(name + "started.");
+    try {
+      wait(1000);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    System.out.println(name + "ended.");
+  }
+
+  public static void main(String[] args) {
+    long begin = System.currentTimeMillis();
+    MyTask task = new MyTask();
     ArrayList<Thread> threadGroup = new ArrayList<Thread>();
-    for(int i = 0; i < 10; i++) {                
-      Thread t = new Thread(task, "Thread" + i); 
-      threadGroup.add(t);                                            
-      t.start();                                                      
-    }                                                                 
-    for(int i = 0; i < threadGroup.size(); i++) {                     
-      Thread t = threadGroup.get(i);                                  
-      try{                                                            
-        t.join();                                                     
-      }catch(Exception e) {                                           
-        e.printStackTrace();;                                         
-      }                                                               
-    }                                                                 
-    System.out.println("Time: " + (System.currentTimeMillis()-begin));
-  }                                                                   
-}                                                                     
-                                                                      
+    for (int i = 0; i < 10; i++) {
+      Thread t = new Thread(task, "Thread" + i);
+      threadGroup.add(t);
+      t.start();
+    }
+    for (int i = 0; i < threadGroup.size(); i++) {
+      Thread t = threadGroup.get(i);
+      try {
+        t.join();
+      } catch (Exception e) {
+        e.printStackTrace();
+        ;
+      }
+    }
+    System.out.println("Time: " + (System.currentTimeMillis() - begin));
+  }
+}
